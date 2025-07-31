ba1e5ddd21c75f78aadda4b5156de52840fb060c
Joel Schroyen <jschroyen@nianticspatial.com>
2025-07-31 15:22:53 +1200

    Tweaks?

--- UnityProject/Assets/MoonSlam/Scripts/Moon.cs
+++ UnityProject/Assets/MoonSlam/Scripts/Moon.cs
@@ -6,10 +6,13 @@ public class Moon : MonoBehaviour
     [SerializeField] private Transform _endPoint;
     [SerializeField] private Transform _startPoint;
     [SerializeField] private Transform _moon;
+    [SerializeField] private AnimationCurve _moveCurve = AnimationCurve.Linear(0,0,1,1);
+    
 
     private void Update()
     {
-        Vector3 newPosition = Vector3.Lerp(_startPoint.position, _endPoint.position, GameManager.Instance.TimeRemainingNormalized);
+        float curvedLerp = _moveCurve.Evaluate(GameManager.Instance.TimeRemainingNormalized);
+        Vector3 newPosition = Vector3.Lerp(_startPoint.position, _endPoint.position, curvedLerp);
         _moon.position = newPosition;
 
     }
