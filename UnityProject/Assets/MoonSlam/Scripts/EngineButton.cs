97ba1a59e8c0cc5878e257200221fcfc0b562cbe
Joel Schroyen <jschroyen@nianticspatial.com>
2025-08-01 16:29:49 +1200

    Ship parts around the place, lighting

--- UnityProject/Assets/MoonSlam/Scripts/EngineButton.cs
+++ UnityProject/Assets/MoonSlam/Scripts/EngineButton.cs
@@ -5,10 +5,16 @@ using UnityEngine.Events;
 
 public class EngineButton : Interactable
 {
+    [SerializeField] private Color _readyColor = Color.green;
+    [SerializeField] private Color _notReadyColor = Color.red;
+    
     private AttachPoint[] _attachPoints;
+
+    private bool _ready;
 
     private void Start()
     {
+        _outline.OutlineColor = _notReadyColor;
         _attachPoints = transform.parent.GetComponentsInChildren<AttachPoint>();
         foreach (AttachPoint attachPoint in _attachPoints)
         {
@@ -18,12 +24,17 @@ public class EngineButton : Interactable
 
     private void AttachPointOnAttached()
     {
+        int numAttached = _attachPoints.Count(attachPoint => attachPoint.IsAttached);
+        if (numAttached == _attachPoints.Length)
+        {
+            _ready = true;
+            _outline.OutlineColor = _readyColor;
+        }
     }
 
     public override void Interact(Interactor interactor)
     {
-        int numAttached = _attachPoints.Count(attachPoint => attachPoint.IsAttached);
-        if (numAttached == _attachPoints.Length)
+        if (_ready)
         {
             GameManager.Instance.EngineActivated();
             base.Interact(interactor);
